b64ae47d740d8a3c4803a49ebce9fd49fbad7921
iqbaaaaal <57170373+IqbaalPratama20@users.noreply.github.com>
2021-01-07 07:15:06 +0700

    fungsi login

--- controller/auth/dbConnection.php
+++ controller/auth/dbConnection.php
@@ -7,6 +7,30 @@
 
     function dbClose() {
         mysli_close(mysqli_connect("localhost", "root", "", "bank_sampah"));
-    }   
+    }
+
+    function loginStaff($request) {
+        global $db;
+
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+
+        $status = mysqli_query($db, "SELECT * FROM staff WHERE s_nama = '$username' AND s_password = '$password'");
+
+        dbclose();
+        return $status;
+    }
+
+    function loginUsers($request) {
+        global $db;
+
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+
+        $status = mysqli_query($db, "SELECT * FROM users WHERE u_username = '$username' AND u_password = '$password'");
+
+        dbclose();
+        return $status;
+    }
 
 ?>
