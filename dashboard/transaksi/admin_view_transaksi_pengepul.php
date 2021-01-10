3f31e6a2f34bbcd2df2b22b2a0121264471e64b7
Alberto0150 <albertosanjaya2@gmail.com>
2021-01-11 00:41:21 +0700

    error error EOF

--- dashboard/transaksi/admin_view_transaksi_pengepul.php
+++ dashboard/transaksi/admin_view_transaksi_pengepul.php
@@ -54,76 +54,77 @@
 				// ambil record
 				while($transaksi = mysqli_fetch_array($all_TransaksiPengepul)){
 			?>
-				<tr>
-                    <td><?php echo $transaksi['t.tp_id']; ?></td>
-                    <td><?php echo $transaksi['p.p_username']; ?></td>
-                    <td><?php echo $transaksi['s.s_nama']; ?></td>
-					<td><?php echo $transaksi['t.tp_tanggal']; ?></td>
-					<!-- LINK DELETE FUNCTION -->
-					<td><?php echo $transaksi['t.tp_ambil']; ?></td>
-					<form method='POST' action='../../controller/dashboard/deleteFunctionTransaksiPengepul.php'>
-                        <input name='userId' value=<?php echo $transaksi['t.tp_id']; ?> hidden/>
-                        <td><button type='submit' class='btn btn-error'>
-                            <i class='fa fa-trash' aria-hidden='true'> Hapus</i>
-                        </button></td>
-					</form>
-					<td class='tableItem'>
-                    <a class="btn btn btn-primary" href="#modalUpdate-<?php echo $transaksi['t.tp_id'];?>"><i class='fa fa-pencil' aria-hidden='true'> Update	
-					</i></a>
-					<div class="modal" id="modalUpdate-<?php echo $transaksi['t.tp_id'];?>">
-                        <a href="#close" class="modal-overlay" aria-label="Close"></a>
-                        <div class="modal-container">
-                            <div class="modal-header">
-                                <a href="#close" class="btn btn-clear float-right" aria-label="Close"></a>
-                            	<div class="modal-title h5">Update Data Guest</div>
-                            </div>
-                            <div class="modal-body">
-								<!-- fungsi update -->
-                            <form action="../../controller/dashboard/updateFunctionTransaksiPengepul.php" method="POST">
-                            <?php
-                                $id = $transaksi['t.tp_id']; 
-                                $query_edit = getAllPengepul("SELECT * FROM transaksi_pengepul  where 'tp_id' ='$id'");
-                                while ($user_edit= mysqli_fetch_array($query_edit)) {  
-                            ?>
-                                <div class="content">
-                                    <input class="form-input" type="hidden" id="id" name="id" value="<?php echo $user_edit['tp_id']; ?>">
-                                    <div class="form-group">
-                                        <div class="text-left"> 
-                                            <label class="form-label" for="id_pengepul">ID Pengepul</label>
-                                        </div>
-                                        <input class="form-input" type="text" id="id_pengepul" name="id_pengepul" value="<?php echo $user_edit['p_id']; ?>"placeholder="ID Pengepul" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="text-left"> 
-                                            <label class="form-label" for="id_staf">ID Staf</label> 
-                                        </div>
-                                        <input class="form-input" type="text" id="id_staf" name="id_staf" value="<?php echo $user_edit['s_id']; ?>"placeholder="ID Staf" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="text-left">
-                                            <label class="form-label" for="Tanggal">Tanggal Transaksi</label>
-                                        </div>  
-                                        <input class="form-input" type="date" id="Tanggal" name="Tanggal" value="<?php echo $user_edit['tp_tanggal']; ?>"placeholder="Tanggal Transaksi" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="text-left">
-                                            <label class="form-label" for="Ambil">Jumlah Ambil</label>
-                                        </div>
-                                        <input class="form-input" type="text" id="Ambil" name="Ambil" value="<?php echo $user_edit['tp_ambil']; ?>"placeholder="Jumlah Ambil" required>
-                                    </div>
-                                </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="submit" class="btn btn-primary">Update</button>
-                                </div>
-                            </form>
-                            <?php 
-                                }
-                            ?> 
-                        </div>
-                    </div>
-					
-				</tr>
+			<tr>
+				<td><?php echo $transaksi['t.tp_id']; ?></td>
+				<td><?php echo $transaksi['p.p_username']; ?></td>
+				<td><?php echo $transaksi['s.s_nama']; ?></td>
+				<td><?php echo $transaksi['t.tp_tanggal']; ?></td>
+				<td><?php echo $transaksi['t.tp_ambil']; ?></td>
+				<form method='POST' action='../../controller/dashboard/deleteFunctionTransaksiPengepul.php'>
+					<input name='userId' value=<?php echo $transaksi['t.tp_id']; ?> hidden>
+					<td><button type='submit' class='btn btn-error'>
+						<i class='fa fa-trash' aria-hidden='true'> Hapus</i>
+					</button></td>
+				</form>
+				<td class='tableItem'>
+				<a class="btn btn btn-primary" href="#modalUpdate-<?php echo $transaksi['t.tp_id'];?>">
+					<i class='fa fa-pencil' aria-hidden='true'> Update	
+					</i>
+				</a>
+				<div class="modal" id="modalUpdate-<?php echo $transaksi['t.tp_id'];?>">
+					<a href="#close" class="modal-overlay" aria-label="Close">
+					</a>
+					<div class="modal-container">
+						<div class="modal-header">
+							<a href="#close" class="btn btn-clear float-right" aria-label="Close"></a>
+							<div class="modal-title h5">Update Data Guest</div>
+						</div>
+						<div class="modal-body">
+							<form action="../../controller/dashboard/updateFunctionTransaksiPengepul.php" method="POST">
+								<?php
+									$id = $transaksi['t.tp_id']; 
+									$query_edit = getAllPengepul("SELECT * FROM transaksi_pengepul  where 'tp_id' ='$id'");
+									while ($user_edit= mysqli_fetch_array($query_edit)) 
+									{  
+								?>
+									<div class="content">
+										<input class="form-input" type="hidden" id="id" name="id" value="<?php echo $user_edit['tp_id']; ?>">
+										<div class="form-group">
+											<div class="text-left"> 
+												<label class="form-label" for="id_pengepul">ID Pengepul</label>
+											</div>
+											<input class="form-input" type="text" id="id_pengepul" name="id_pengepul" value="<?php echo $user_edit['p_id']; ?>"placeholder="ID Pengepul" required>
+										</div>
+										<div class="form-group">
+											<div class="text-left"> 
+												<label class="form-label" for="id_staf">ID Staf</label> 
+											</div>
+											<input class="form-input" type="text" id="id_staf" name="id_staf" value="<?php echo $user_edit['s_id']; ?>"placeholder="ID Staf" required>
+										</div>
+										<div class="form-group">
+											<div class="text-left">
+												<label class="form-label" for="Tanggal">Tanggal Transaksi</label>
+											</div>  
+											<input class="form-input" type="date" id="Tanggal" name="Tanggal" value="<?php echo $user_edit['tp_tanggal']; ?>"placeholder="Tanggal Transaksi" required>
+										</div>
+										<div class="form-group">
+											<div class="text-left">
+												<label class="form-label" for="Ambil">Jumlah Ambil</label>
+											</div>
+											<input class="form-input" type="text" id="Ambil" name="Ambil" value="<?php echo $user_edit['tp_ambil']; ?>"placeholder="Jumlah Ambil" required>
+										</div>
+									</div>
+									<div class="modal-footer">
+										<button type="submit" class="btn btn-primary">Update</button>
+									</div>
+								<?php 
+									}
+								?> 
+							</form>
+						</div>
+					</div>
+				</div>
+			</tr>
 		</div>
 	</div>		
 	<?php
@@ -133,3 +134,4 @@
         // }
     ?>
 </body>
+</html>
