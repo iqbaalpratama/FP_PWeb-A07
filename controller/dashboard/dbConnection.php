6d9d881df3f26a98d600968c59e1d0e84bf79f3f
syubban <syubbanfakhriya@gmail.com>
2021-01-10 19:34:25 +0700

    adding pengguna feature

--- controller/dashboard/dbConnection.php
+++ controller/dashboard/dbConnection.php
@@ -1,12 +1,20 @@
-<?
+<?php
 
     $db = mysqli_connect("localhost", "root", "", "bank_sampah");
     if( !$db ) {
         die("Gagal terhubung dengan database: " . mysqli_connect_error());
     }
 
+    function getAllStaff($query) {
+        global $db;
+        $all_staff = mysqli_query($db, $query);
+
+        dbclose();
+        return $all_staff;
+    }
+
     function dbClose() {
-        mysli_close(mysqli_connect("localhost", "root", "", "bank_sampah"));
+        mysqli_close(mysqli_connect("localhost", "root", "", "bank_sampah"));
     }   
 
 ?>
