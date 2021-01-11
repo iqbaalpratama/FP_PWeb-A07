b09447ee18d4a40c023d5bd01422470bf1495466
syubban <syubbanfakhriya@gmail.com>
2021-01-11 21:32:19 +0700

    crud staff

--- controller/dashboard/dbConnection.php
+++ controller/dashboard/dbConnection.php
@@ -5,12 +5,53 @@
         die("Gagal terhubung dengan database: " . mysqli_connect_error());
     }
 
-    function getAllStaff($query) {
+    function getAll($query) {
         global $db;
         $all_staff = mysqli_query($db, $query);
 
         dbclose();
         return $all_staff;
+    }
+
+    function updateStaff($request) {
+        global $db;
+
+        $s_id = $_POST['s_id'];
+        $s_nama = $_POST['s_nama'];
+        $bank = $_POST['bank'];
+        $s_alamat = $_POST['s_alamat'];
+        $s_telepon = $_POST['s_telepon'];
+        
+        mysqli_query($db, "UPDATE staff SET b_id = '$bank', s_nama = '$s_nama', s_alamat = '$s_alamat', s_telepon = '$s_telepon' WHERE s_id = '$s_id'");
+        $status = mysqli_affected_rows($db);
+        dbclose();
+        return $status;
+    }
+
+    function createStaff($request) {
+        global $db;
+
+        $s_id = $_POST['s_id'];
+        $s_nama = $_POST['s_nama'];
+        $bank = $_POST['bank'];
+        $s_alamat = $_POST['s_alamat'];
+        $s_telepon = $_POST['s_telepon'];
+        
+        mysqli_query($db, "INSERT INTO staff (b_id, s_nama, s_alamat, s_telepon) VALUES ('$bank', '$s_nama', '$s_alamat', '$s_telepon')");
+        $status = mysqli_affected_rows($db);
+        dbclose();
+        return $status;
+    }
+
+    function deleteStaff($request) {
+        global $db;
+
+        $s_id = $_POST['s_id'];
+        
+        mysqli_query($db, "DELETE FROM staff WHERE s_id = '$s_id'");
+        $status = mysqli_affected_rows($db);
+        dbclose();
+        return $status;
     }
 
     function dbClose() {
--- dashboard/pengguna/staff.php
+++ dashboard/pengguna/staff.php
@@ -1,6 +1,6 @@
 <?php
 	require '../../controller/dashboard/dbConnection.php';
-	$all_staff = getAllStaff("SELECT * FROM staff");
+	$all_staff = getAll("SELECT * FROM staff INNER JOIN cabang_bank ON staff.b_id = cabang_bank.b_id");
 ?>
 
 <!doctype html>
@@ -55,36 +55,11 @@
 			</div>
 		</nav>
 		<!-- END NAVBAR -->
-		<!-- LEFT SIDEBAR -->
-		<div id="sidebar-nav" class="sidebar">
-			<div class="sidebar-scroll">
-				<nav>
-					<ul class="nav">
-						<li><a href="index.html" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
-						<li><a href="elements.html" class=""><i class="lnr lnr-code"></i> <span>Elements</span></a></li>
-						<li><a href="charts.html" class=""><i class="lnr lnr-chart-bars"></i> <span>Charts</span></a></li>
-						<li><a href="panels.html" class=""><i class="lnr lnr-cog"></i> <span>Panels</span></a></li>
-						<li><a href="../../dashboard/pengguna/pengguna.php" class=""><i class="lnr lnr-alarm"></i> <span>Pengguna</span></a></li>
-						<li>
-							<a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> 
-								<span>Data Pengguna</span> <i class="icon-submenu lnr lnr-chevron-left"></i>
-							</a>
-							<div id="subPages" class="collapse ">
-								<ul class="nav">
-									<li><a href="page-profile.html" class="">Staff</a></li>
-									<li><a href="page-login.html" class="">Nasabah</a></li>
-									<li><a href="page-lockscreen.html" class="">Pengepul</a></li>
-								</ul>
-							</div>
-						</li>
-						<li><a href="tables.html" class=""><i class="lnr lnr-dice"></i> <span>Tables</span></a></li>
-						<li><a href="typography.html" class=""><i class="lnr lnr-text-format"></i> <span>Typography</span></a></li>
-						<li><a href="icons.html" class=""><i class="lnr lnr-linearicons"></i> <span>Icons</span></a></li>
-					</ul>
-				</nav>
-			</div>
-		</div>
-		<!-- END LEFT SIDEBAR -->
+		
+		<?php
+			include('../sidebar.php');
+		?>
+		
 		<!-- MAIN -->
 		<div class="main">
 			<!-- MAIN CONTENT -->
@@ -104,7 +79,7 @@
 										<thead>
 											<tr>
 												<th>s_id</th>
-												<th>b_id</th>
+												<th>b_nama</th>
 												<th>s_nama</th>
 												<th>s_alamat</th>
 												<th>s_telepon</th>
@@ -118,16 +93,125 @@
 										?>
 											<tr>
 												<td><?php echo $staff['s_id']; ?></td>
-												<td><?php echo $staff['b_id']; ?></td>
+												<td><?php echo $staff['b_nama']; ?></td>
 												<td><?php echo $staff['s_nama']; ?></td>
 												<td><?php echo $staff['s_alamat']; ?></td>
 												<td><?php echo $staff['s_telepon']; ?></td>
+												<td>
+													<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalUpdate-<?php echo $staff['s_id']; ?>">Edit Staff</button>
+													<div class="modal fade" id="modalUpdate-<?php echo $staff['s_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="modalUpdateLabel" aria-hidden="true">
+														<div class="modal-dialog" role="document">
+															<div class="modal-content">
+																<div class="modal-header">
+																	<h5 class="modal-title" id="exampleModalLabel">Update</h5>
+																	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																		<span aria-hidden="true">&times;</span>
+																	</button>
+																</div>
+																<form method="POST" action="../../controller/dashboard/pengguna/editStaff.php">
+																	<div class="modal-body">
+																		<div class="input-group col-md-12" style="margin-top: 15px">
+																			<input class="form-control" type="text" name="s_id" id="s_id" value="<?php echo $staff['s_id']; ?>">
+																		</div>
+																		<div class="input-group col-md-12" style="margin-top: 15px">
+																			<input class="form-control" type="text" name="s_nama" id="s_nama" placeholder="nama" value="<?php echo $staff['s_nama']; ?>">
+																		</div>
+																		<div class="input-group col-md-12" style="margin-top: 15px">
+																			<select class="form-control" name="bank" id="bank">
+																				<?php
+																					$all_bank = getAll("SELECT * FROM cabang_bank");
+																					while($bank = mysqli_fetch_array($all_bank)) {
+																				?>
+																					<?php 
+																						if($bank['b_nama'] == $staff['b_nama']) {
+																					?>
+																						<option value="<?php echo $bank['b_id']?>" selected="selected"><?php echo $bank['b_nama']?></option>
+																					<?php 
+																						} else {
+																					?>
+																						<option value="<?php echo $bank['b_id']?>"><?php echo $bank['b_nama']?></option>
+																					<?php 
+																						}
+																					?>
+																				<?php
+																					}
+																				?>
+																			</select>
+																		</div>
+																		<div class="input-group col-md-12" style="margin-top: 15px">
+																			<input class="form-control" type="text" name="s_alamat" id="s_alamat" placeholder="alamat" value="<?php echo $staff['s_alamat']; ?>">
+																		</div>
+																		<div class="input-group col-md-12" style="margin-top: 15px">
+																			<input class="form-control" type="text" name="s_telepon" id="s_telepon" placeholder="telepon" value="<?php echo $staff['s_telepon']; ?>">
+																		</div>
+																	</div>
+																	<div class="modal-footer">
+																		<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+																		<button class="btn btn-primary" type="submit">Save changes</button>
+																	</div>
+																</form>
+															</div>
+														</div>
+													</div>
+												</td>
+												<td>
+													<form method="POST" action="../../controller/dashboard/pengguna/deleteStaff.php">
+														<div class="input-group">
+															<input class="form-control" type="text" name="s_id" id="s_id" value="<?php echo $staff['s_id']; ?>" style="display: none;">
+														</div>
+														<button class="btn btn-danger" type="submit">Delete Staff</button>
+													</form>
+												</td>
 											</tr>
 										<?php
 											}
 										?>
 										</tbody>
 									</table>
+									<div style="flex: 1;">
+										<button type="button" class="btn btn-success" data-toggle="modal" data-target="#createModal">Tambah</button>
+										<div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="modalCreateLabel" aria-hidden="true">
+											<div class="modal-dialog" role="document">
+												<div class="modal-content">
+													<div class="modal-header">
+														<h5 class="modal-title" id="exampleModalLabel">Update</h5>
+														<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+															<span aria-hidden="true">&times;</span>
+														</button>
+													</div>
+													<form method="POST" action="../../controller/dashboard/pengguna/createStaff.php">
+														<div class="modal-body">
+															<div class="input-group col-md-12" style="margin-top: 15px">
+																<input class="form-control" type="text" name="s_nama" id="s_nama" placeholder="nama">
+															</div>
+															<div class="input-group col-md-12" style="margin-top: 15px">
+																<select class="form-control" name="bank" id="bank">
+																	<?php
+																		$all_bank = getAll("SELECT * FROM cabang_bank");
+																		while($bank = mysqli_fetch_array($all_bank)) {
+																	?>
+																		<option value="<?php echo $bank['b_id']?>"><?php echo $bank['b_nama']?></option>
+																	<?php
+																		}
+																	?>
+																</select>
+															</div>
+															<div class="input-group col-md-12" style="margin-top: 15px">
+																<input class="form-control" type="text" name="s_alamat" id="s_alamat" placeholder="alamat">
+															</div>
+															<div class="input-group col-md-12" style="margin-top: 15px">
+																<input class="form-control" type="text" name="s_telepon" id="s_telepon" placeholder="telepon">
+															</div>
+														</div>
+														<div class="modal-footer">
+															<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+															<button class="btn btn-primary" type="submit">Save changes</button>
+														</div>
+													</form>
+												</div>
+											</div>
+										</div>
+									</div>
 								</div>
 							</div>
 							<!-- END TABLE HOVER -->
@@ -153,124 +237,6 @@
 	<script src="../../assets/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
 	<script src="../../assets/vendor/chartist/js/chartist.min.js"></script>
 	<script src="../../assets/scripts/klorofil-common.js"></script>
-	<script>
-	$(function() {
-		var data, options;
-
-		// headline charts
-		data = {
-			labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-			series: [
-				[23, 29, 24, 40, 25, 24, 35],
-				[14, 25, 18, 34, 29, 38, 44],
-			]
-		};
-
-		options = {
-			height: 300,
-			showArea: true,
-			showLine: false,
-			showPoint: false,
-			fullWidth: true,
-			axisX: {
-				showGrid: false
-			},
-			lineSmooth: false,
-		};
-
-		new Chartist.Line('#headline-chart', data, options);
-
-
-		// visits trend charts
-		data = {
-			labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-			series: [{
-				name: 'series-real',
-				data: [200, 380, 350, 320, 410, 450, 570, 400, 555, 620, 750, 900],
-			}, {
-				name: 'series-projection',
-				data: [240, 350, 360, 380, 400, 450, 480, 523, 555, 600, 700, 800],
-			}]
-		};
-
-		options = {
-			fullWidth: true,
-			lineSmooth: false,
-			height: "270px",
-			low: 0,
-			high: 'auto',
-			series: {
-				'series-projection': {
-					showArea: true,
-					showPoint: false,
-					showLine: false
-				},
-			},
-			axisX: {
-				showGrid: false,
-
-			},
-			axisY: {
-				showGrid: false,
-				onlyInteger: true,
-				offset: 0,
-			},
-			chartPadding: {
-				left: 20,
-				right: 20
-			}
-		};
-
-		new Chartist.Line('#visits-trends-chart', data, options);
-
-
-		// visits chart
-		data = {
-			labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-			series: [
-				[6384, 6342, 5437, 2764, 3958, 5068, 7654]
-			]
-		};
-
-		options = {
-			height: 300,
-			axisX: {
-				showGrid: false
-			},
-		};
-
-		new Chartist.Bar('#visits-chart', data, options);
-
-
-		// real-time pie chart
-		var sysLoad = $('#system-load').easyPieChart({
-			size: 130,
-			barColor: function(percent) {
-				return "rgb(" + Math.round(200 * percent / 100) + ", " + Math.round(200 * (1.1 - percent / 100)) + ", 0)";
-			},
-			trackColor: 'rgba(245, 245, 245, 0.8)',
-			scaleColor: false,
-			lineWidth: 5,
-			lineCap: "square",
-			animate: 800
-		});
-
-		var updateInterval = 3000; // in milliseconds
-
-		setInterval(function() {
-			var randomVal;
-			randomVal = getRandomInt(0, 100);
-
-			sysLoad.data('easyPieChart').update(randomVal);
-			sysLoad.find('.percent').text(randomVal);
-		}, updateInterval);
-
-		function getRandomInt(min, max) {
-			return Math.floor(Math.random() * (max - min + 1)) + min;
-		}
-
-	});
-	</script>
 </body>
 
 </html>
