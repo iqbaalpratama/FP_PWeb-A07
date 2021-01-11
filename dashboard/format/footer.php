21cf3c3d6fb534e3e43bef98be7e6d428dc86b08
Alberto0150 <albertosanjaya2@gmail.com>
2021-01-11 23:00:13 +0700

    update dikit benerin view

--- dashboard/format/footer.php
+++ dashboard/format/footer.php
@@ -3,9 +3,9 @@
         <p class="copyright">&copy; 2017 <a href="https://www.themeineed.com" target="_blank">Theme I Need</a>. All Rights Reserved.</p>
     </div>
 </footer>
-<script src="../assets/vendor/jquery/jquery.min.js"></script>
-<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="../assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script src="../assets/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
-<script src="../assets/vendor/chartist/js/chartist.min.js"></script>
-<script src="../assets/scripts/klorofil-common.js"></script>
+<script src="../../assets/vendor/jquery/jquery.min.js"></script>
+<script src="../../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../../assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="../../assets/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+<script src="../../assets/vendor/chartist/js/chartist.min.js"></script>
+<script src="../../assets/scripts/klorofil-common.js"></script>
