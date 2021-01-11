1b71ef43a4ba4c00e283284fa119c4cc04d6d3b7
Alberto0150 <albertosanjaya2@gmail.com>
2021-01-11 20:48:50 +0700

    ?

--- controller/auth/loginStaff.php
+++ controller/auth/loginStaff.php
@@ -5,7 +5,7 @@
     if(mysqli_num_rows(loginStaff(isset($_POST['Submit']))) == 1){
         $_SESSION["username"] = true;
         $_SESSION["role"] = "Staff";
-        // header('Location: ../view.php');
+        header('Location: ../dashboard/pengguna/staff.php');
     }else {
         $_SESSION["gagal"] = "Gagal melakukan login";
         // header('Location: ../dashboardlogin.php');
--- dashboard/pengguna/staff.php
+++ dashboard/pengguna/staff.php
@@ -4,7 +4,7 @@
 ?>
 
 <?php require_once("../format/head_format_start.php"); ?>
-	<title>Pengguna</title>
+	<title>Dashboard Staff</title>
 <?php require_once("../format/head_format_end.php"); ?>
 
 <body>
@@ -40,26 +40,16 @@
 			<div class="sidebar-scroll">
 				<nav>
 					<ul class="nav">
-						<li><a href="index.html" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
-						<li><a href="elements.html" class=""><i class="lnr lnr-code"></i> <span>Elements</span></a></li>
-						<li><a href="charts.html" class=""><i class="lnr lnr-chart-bars"></i> <span>Charts</span></a></li>
-						<li><a href="panels.html" class=""><i class="lnr lnr-cog"></i> <span>Panels</span></a></li>
-						<li><a href="../../dashboard/pengguna/pengguna.php" class=""><i class="lnr lnr-alarm"></i> <span>Pengguna</span></a></li>
-						<li>
-							<a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> 
-								<span>Data Pengguna</span> <i class="icon-submenu lnr lnr-chevron-left"></i>
-							</a>
-							<div id="subPages" class="collapse ">
-								<ul class="nav">
-									<li><a href="page-profile.html" class="">Staff</a></li>
-									<li><a href="page-login.html" class="">Nasabah</a></li>
-									<li><a href="page-lockscreen.html" class="">Pengepul</a></li>
-								</ul>
-							</div>
-						</li>
-						<li><a href="tables.html" class=""><i class="lnr lnr-dice"></i> <span>Tables</span></a></li>
-						<li><a href="typography.html" class=""><i class="lnr lnr-text-format"></i> <span>Typography</span></a></li>
-						<li><a href="icons.html" class=""><i class="lnr lnr-linearicons"></i> <span>Icons</span></a></li>
+						<!-- dashboard -->
+						<li><a href="#" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
+						<!-- view user  -->
+						<li><a href="#" class=""><i class="lnr lnr-code"></i> <span>Daftar User</span></a></li>
+						<!-- view pengepul -->
+						<li><a href="#" class=""><i class="lnr lnr-chart-bars"></i> <span>Daftar Pengepul</span></a></li>
+						<!--  view transaksi user -->
+						<li><a href="../admin_view_transaksi_user.php" class=""><i class="lnr lnr-alarm"></i> <span>Transaksi User</span></a></li>
+						<!-- view transaksi pengepul -->
+						<li><a href="../admin_view_transaksi_pengepul.php" class=""><i class="lnr lnr-cog"></i> <span>Transaksi Pengepul</span></a></li>
 					</ul>
 				</nav>
 			</div>
