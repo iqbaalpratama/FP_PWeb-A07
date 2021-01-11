54de5a240d41ac6f98f99c110a274edae76c2228
iqbaaaaal <57170373+IqbaalPratama20@users.noreply.github.com>
2021-01-11 11:08:31 +0700

    add login register users, login staff. minus register staff

--- controller/auth/dbConnection.php
+++ controller/auth/dbConnection.php
@@ -33,4 +33,55 @@
         return $status;
     }
 
+    function registerStaff($request) {
+        global $db;
+
+        $username=$_POST['nama'];
+        $password=md5($_POST['password']);
+        $alamat=$_POST['alamat'];
+        $no_telp=$_POST['no_telp'];
+        $bank_id=$_POST['bank_id'];
+        mysqli_query($koneksi,"INSERT INTO user(s_nama, s_password, s_alamat, s_telepon, s_id) VALUES('$username','$password', '$alamat', '$no_telp', '$id_bank')");
+        $status = mysqli_affected_rows($db);
+
+        dbclose();
+        return $status;
+    }
+
+    function registerUsers($request) {
+        global $db;
+        $username=$_POST['username'];
+        $password=md5($_POST['password']);
+        $alamat=$_POST['alamat'];
+        $no_telp=$_POST['no_telp'];
+        if(!$_FILES || !key_exists('file', $_FILES)){
+            echo('File tidak terupload.');
+        }
+
+        $temporaryFile = $_FILES['file']['tmp_name'];
+        $extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+
+        // Check the MIME type
+        $supportedFormat = ['image/jpg', 'image/jpeg', 'image/png'];
+        if(!in_array(mime_content_type($temporaryFile), $supportedFormat)){
+            echo('Unggah file berekstensi JPG, JPEG, atau PNG.');
+        }
+
+        // Cek ukuran gambar
+        if(getimagesize($temporaryFile) == 0){
+            echo('Ukuran tidak valid.');
+        }
+
+        // Upload gambar
+        $filename = md5($temporaryFile).'.'.$extension;
+        // echo($filename);
+        move_uploaded_file($temporaryFile, '../upload/'.$filename);
+
+        mysqli_query($koneksi,"INSERT INTO users(u_username, u_password, u_alamat, u_telp, u_foto) VALUES('$username','$password', '$alamat', '$no_telp', '$filename')");
+        $status = mysqli_affected_rows($db);
+
+        dbclose();
+        return $status;
+    }
+
 ?>
---
+++ controller/auth/loginStaff.php
@@ -0,0 +1,14 @@
+<?php
+    include 'dbConnection.php';
+    session_start();
+
+    if(mysqli_num_rows(loginStaff(isset($_POST['Submit']))) == 1){
+        $_SESSION["username"] = true;
+        $_SESSION["role"] = "Staff";
+        // header('Location: ../view.php');
+    }else {
+        $_SESSION["gagal"] = "Gagal melakukan login";
+        // header('Location: ../dashboardlogin.php');
+    }
+    exit();
+?>
